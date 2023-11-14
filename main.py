9356d043bf494c6f3598fa9a40099312fe4b4897
Gilmar José <99229360+g42puts@users.noreply.github.com>
2023-11-14 00:49:03 -0300

    Update main.py
    Fiz algumas alterações em relação a indentação para melhor legibilidade e removi algumas importações duplicadas.

--- main.py
+++ main.py
@@ -1,52 +1,59 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome import options
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver import Chrome
-import time
 from bs4 import BeautifulSoup as bs
+
+from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver import ActionChains
 from selenium.webdriver.common.actions.action_builder import ActionBuilder
 from selenium.webdriver.common.actions.mouse_button import MouseButton
 from selenium.webdriver.common.by import By
-from selenium import webdriver
-from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+
 from datetime import datetime, timedelta
+import time
 import shutil
 import os
 
+
 options = Options()
+
 options.headless = False #Excutar de forma visivel
+
 
 ##Acessando o navegador
 navegador = webdriver.Chrome(options=options)
+
 
 ##Link para acessar o site
 link= "https://idash.ifcshop.net/auth/realms/chat-bot-b2b/protocol/openid-connect/auth?client_id=chat-bot-b2b-app&redirect_uri=https%3A%2F%2Fidash.ifcshop.net%2Freports%2Fattendance-quality&state=1a486ee2-1410-4ae0-887a-6220d195130b&response_mode=fragment&response_type=code&scope=openid&nonce=c559ad80-4974-4ef3-b331-5cc14bd83daa"
 navegador.get(url=link)
 time.sleep(3)
 
+
 ##Inserindo usuario
 inputUsuario = navegador.find_element(by=By.ID,value="username")
 inputUsuario.send_keys("********")
 time.sleep(5)
+
 
 ##Inserindo senha
 inputSenha = navegador.find_element(by=By.ID,value="password")
 inputSenha.send_keys("******")
 time.sleep(5)
 
+
 ##Clicando no botão Sign In
 buttonLogin = navegador.find_element(by=By.ID,value="kc-login")
 buttonLogin.click()
 time.sleep(5)
 
+
 ##Acessando a pagina quality
 linkQ="https://idash.ifcshop.net/reports/attendance-quality"
 navegador.get(url=linkQ)
 time.sleep(5)
+
 
 ##Clicando no botão Selecionar todos
 clickable = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
@@ -60,12 +67,14 @@ time.sleep(10)
 data_ant = datetime.now() - timedelta(days=1)
 data_ant_form = data_ant.strftime("%d/%m/%Y")
 
+
 ##Preenchendo data anterior quality
 text_input = navegador.find_element(By.CSS_SELECTOR, "#\:r2\:")
 ActionChains(navegador)\
 .send_keys_to_element(text_input, data_ant_form)\
 .perform()
 time.sleep(10)
+
 
 ##Clicando no botão para gerar o relatório
 GerarRelatorio = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
@@ -74,18 +83,22 @@ ActionChains(navegador)\
 .perform()
 time.sleep(20)
 
+
 ##mover arquivo
 download_dir = r'C:\Users\Peter\Downloads'
-qt=os.listdir(download_dir)
+qt = os.listdir(download_dir)
 arquivo_mais_recente = max(qt, key=lambda f: os.path.getctime(os.path.join(download_dir, f)))
-CaminhoArquivo = os.path.join(download_dir,arquivo_mais_recente)
+CaminhoArquivo = os.path.join(download_dir, arquivo_mais_recente)
 pasta_destino= r'C:\Users\Peter\TA'
 shutil.move(CaminhoArquivo,os.path.join(pasta_destino,arquivo_mais_recente))
 time.sleep(10)
+
+
 ##Acessando a pagina Retenção    
 linkR="https://idash.ifcshop.net/reports/subject"
 navegador.get(url=linkR)
 time.sleep(5)
+
 
 ##Clicando no botão Selecionar todos
 clickable1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
@@ -94,6 +107,7 @@ ActionChains(navegador)\
 .perform()
 time.sleep(10)
 
+
 ##Preenchendo data anterior Retenção
 text_input1 = navegador.find_element(By.CSS_SELECTOR,value= "#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(3) > div > div")
 ActionChains(navegador)\
@@ -101,12 +115,14 @@ ActionChains(navegador)\
 .perform()
 time.sleep(10)
 
+
 ##Clicando no botão para gerar o relatório
 GerarRelatorio1 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
 ActionChains(navegador)\
 .click(GerarRelatorio1)\
 .perform()
 time.sleep(35)
+
 
 ##mover arquivo
 download_dir = r'C:\Users\Peter\Downloads'
@@ -117,10 +133,12 @@ pasta_destino1= r'C:\Users\Peter\TA1'
 shutil.move(CaminhoArquivo,os.path.join(pasta_destino1,arquivo_mais_recente))
 time.sleep(10)
 
+
 ##Acessando a pagina Tickets    
 linkT="https://idash.ifcshop.net/reports/tickets"
 navegador.get(url=linkT)
 time.sleep(5)
+
 
 ##Clicando no botão Selecionar todos
 clickable2 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(1) > div > div > button')
@@ -129,6 +147,7 @@ ActionChains(navegador)\
 .perform()
 time.sleep(10)
 
+
 ##Preenchendo data anterior Tickets
 text_input2 = navegador.find_element(By.CSS_SELECTOR,value= "#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div:nth-child(3) > div > div")
 ActionChains(navegador)\
@@ -136,12 +155,14 @@ ActionChains(navegador)\
 .perform()
 time.sleep(10)
 
+
 ##Clicando no botão para gerar o relatório
 GerarRelatorio2 = navegador.find_element(By.CSS_SELECTOR,value= '#root > div > main > div.MuiContainer-root.css-10ur324 > div.MuiGrid-root.MuiGrid-container.MuiGrid-spacing-xs-2.css-isbt42 > div.MuiGrid-root.MuiGrid-item.MuiGrid-grid-xs-12.css-1a9w728 > button')
 ActionChains(navegador)\
 .click(GerarRelatorio2)\
 .perform()
 time.sleep(50)
+
 
 ##mover arquivo
 download_dir = r'C:\Users\Peter\Downloads'
@@ -151,4 +172,3 @@ CaminhoArquivo = os.path.join(download_dir,arquivo_mais_recente)
 pasta_destino2= r'C:\Users\Peter\TA2'
 shutil.move(CaminhoArquivo,os.path.join(pasta_destino2,arquivo_mais_recente))
 time.sleep(10)
-
